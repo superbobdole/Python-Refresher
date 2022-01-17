99b8e7850151b6df5b19539a37d4ef1fd11ad140
superbobdole <38264354+superbobdole@users.noreply.github.com>
2022-01-17 11:27:08 -0800

    cool

--- 02_string_formatting/string.py
+++ 02_string_formatting/string.py
@@ -1,2 +1,4 @@
 x = "protego"
 print("Value: " + x + "Length: ", len(x))
+print("Value: {} Length: {}".format(x, len(x)
+print(f"Value: {x} Length: {len(x)}")
--- 13_dictionaries/keys_and_values.py
+++ 13_dictionaries/keys_and_values.py
@@ -1,13 +1,14 @@
 friend_ages = {"Rolf": 24, "Adam": 30, "Anne": 27}
 
 print(friend_ages["Adam"])
-
+# you cannot call a number in dictionaries, you have to call the key
 friend_ages["Bob"] = 43 # add an entry
+# - ["Bob"] is subscript notation
 
-print(friend_ages)
+print(f"Friends Ages: {friend_ages}")
 
 # edit an entry
 
 friend_ages["Adam"] = 69
 
-print(f"Friends age ")
+print(f"Friends Ages -Edited-: {friend_ages}")

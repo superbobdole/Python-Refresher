bcfd9bbede6d7c5317a475f2d80797cb1cfb58e3
superbobdole <38264354+superbobdole@users.noreply.github.com>
2022-01-18 11:11:29 -0800

    cool

--- 19_lambda_functions/doubles_and_map.py
+++ 19_lambda_functions/doubles_and_map.py
@@ -1,7 +1,13 @@
 def double(x):
     return x * 2
 
+
 sequence = [1, 3, 5, 9]
 doubled = [double(x) for x in sequence]
-# or
+# map can do the same thing as this list comprehension
+# list comprehension is faster
 doubled = map(double, sequence)
+# lambda style
+doubled = map(lambda x: x * 2, sequence)
+# to print
+doubled = list(map(lambda x: x * 2, sequence))

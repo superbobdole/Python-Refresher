e0f5ee2c26c3bd02ba8750e6382002e8dce0a1d6
superbobdole <38264354+superbobdole@users.noreply.github.com>
2022-01-24 15:31:53 -0800

    cool

---
+++ 28_type_hinting/code.py
@@ -0,0 +1,13 @@
+from typing import List
+
+Class Book
+    pass
+
+
+class BookShelf:
+    def __init__(self, books: List[Book]):
+        self.books = books
+
+    def __str__(self) -> str:
+        return f"BookShelf with {len(self.books)} books."
+
